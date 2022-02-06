47fa176611e05c2fbbf52dbad5696f01c887c0bb
zZeTraL <lucashbs.pro@gmail.com>
2022-02-06 23:40:12 +0100

    🌠 Update - Login and Register form (template) is now working - Switch between login and register form work - Some improvements

--- public/js/login/main.js
+++ public/js/login/main.js
@@ -1,3 +1,37 @@
-window.onload = function(){
+window.onload = function () {
+
+    // Need to be cleaner
+    if (loginManager.getUrlParameter("signup") === "true") {
+        // Update the display
+        let loginTitleContainer = document.querySelector('.login__title');
+        loginTitleContainer.childNodes[1].childNodes[3].textContent = "HI YOU"
+
+        let mark = document.createElement("mark");
+        loginTitleContainer.childNodes[3].appendChild(mark);
+        loginTitleContainer.childNodes[3].firstChild.textContent = "Create an account"
+
+        document.querySelector(".login__button").textContent = "Sign Up"
+
+        let signupContainer = document.querySelector(".signup")
+        signupContainer.childNodes[1].remove();
+        let p = document.createElement("p");
+        p.textContent = "Already has an account? ";
+        signupContainer.append(p);
+
+        let span = document.createElement("span");
+        span.setAttribute("id", "registerSpan");
+        span.textContent = "Sign In";
+        p.appendChild(span);
+
+        let i = document.createElement("i");
+        i.setAttribute("class", "bx bx-right-arrow-alt");
+        span.appendChild(i);
+
+
+    }
+
+    document.getElementById("registerSpan").addEventListener('click', (ignored) => {
+        loginManager.onRegisterSpanClick();
+    })
 
 }
--- public/js/login/manager/LoginManager.js
+++ public/js/login/manager/LoginManager.js
@@ -1,10 +1,33 @@
 let loginManager = (function() {
-
+    // Some importants variable
     let usernameInput = document.getElementById("usernameInput");
     let passwordInput = document.getElementsByName("passwordInput");
+    let registerSpan = document.getElementById("registerSpan");
 
     return {
+        // Getters
+        getCurrentUrl: () => window.location.href,
+        getUrlParameter(parameterName){
+          let tmp = new URLSearchParams(window.location.search);
+          return tmp.get(parameterName);
+        },
 
+        // Setters
+        setUrlParameter(parameterName, value){
+            let tmp =  new URLSearchParams(window.location.search);
+            tmp.set(parameterName, value);
+            history.replaceState(null, null, "?" + tmp.toString());
+            location.reload();
+        },
+
+        // Listener
+        onRegisterSpanClick(){
+            let signupForm = this.getUrlParameter("signup");
+            if(signupForm === "false"){
+                this.setUrlParameter("signup", "true");
+            } else {
+                this.setUrlParameter("signup", "false");
+            }
+        }
     }
-
 })();
