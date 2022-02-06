5efc47a8cecc02a53790bc86d6dffd47fedfa88d
zZeTraL <lucashbs.pro@gmail.com>
2022-02-06 05:29:34 +0100

    🌠 Update - CSS for login.html is now finished - This page is also fully responsive - Some improvements

---
+++ public/js/login/manager/LoginManager.js
@@ -0,0 +1,10 @@
+let loginManager = (function() {
+
+    let usernameInput = document.getElementById("usernameInput");
+    let passwordInput = document.getElementsByName("passwordInput");
+
+    return {
+
+    }
+
+})();
--- routes/app.js
+++ routes/app.js
@@ -11,7 +11,7 @@ const localEnv = {
 app.use(express.static(path.join(__dirname, "..")));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "views", "hover", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "views", "home", "index.html"));
 })
 
 http.listen(localEnv.port, () => {
