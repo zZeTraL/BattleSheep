b9fbbcc8d0cd434beae5b4f7dad0794ee55ef3b4
zZeTraL <lucashbs.pro@gmail.com>
2022-02-10 01:36:31 +0100

    🌠 Update - Enhance mobile navigation on header
    TODO
    - Refactoring login methods (with mysql)
    - Check if user's email is valid

--- public/js/responsive/HeaderResponsive.js
+++ public/js/responsive/HeaderResponsive.js
@@ -1,6 +1,6 @@
 window.onload = function () {
+    let navContainer = document.getElementById("navContainer");
     document.getElementById("navList").addEventListener('click', (ignored) => {
-        let navContainer = document.getElementById("navContainer");
         let navStyle = navContainer.style;
         if (navStyle.display === "flex") {
             navStyle.display = "none";
@@ -9,3 +9,15 @@ window.onload = function () {
         }
     })
 }
+
+let media = window.matchMedia("(min-width: 65em)")
+let navContainer = document.getElementById("navContainer");
+const headerNavList = new ResizeObserver((entries) => {
+    let navStyle = navContainer.style;
+    if(media.matches){
+        navStyle.display = "flex";
+    } else {
+        navStyle.display = "none";
+    }
+});
+headerNavList.observe(document.body);
